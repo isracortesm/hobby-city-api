124edefa9d4f034e6fab3d02d0642de6341cf539
ICORTESPC\just_ <isra.cortes.m@gmail.com>
2026-06-11 12:37:43 -0600

    configure resend emails with validated domain

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,20 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+
+    email: {
+        config: {
+            provider: 'strapi-provider-email-resend',
+            providerOptions: {
+                apiKey: env('RESEND_API_KEY'), // Required
+            },
+            settings: {
+                defaultFrom: env('RESEND_EMAIL_FROM'),
+                defaultReplyTo: env('RESEND_EMAIL_REPLY'),
+            },
+        }
+    },    
+
+});
 
 export default config;
--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,4 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const config = ({ env }) => ({});
+const config = ({ env }) => ({
+    email: {
+        config: {
+            provider: 'strapi-provider-email-resend',
+            providerOptions: {
+                apiKey: env('RESEND_API_KEY'), // Required
+            },
+            settings: {
+                defaultFrom: env('RESEND_EMAIL_FROM'),
+                defaultReplyTo: env('RESEND_EMAIL_REPLY'),
+            },
+        }
+    },
+});
 exports.default = config;
