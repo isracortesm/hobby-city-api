a94a83082cc1f2722ff946b4eb7a734f4d06159e
ICORTESPC\just_ <isra.cortes.m@gmail.com>
2026-06-11 21:45:03 -0600

    config media storage by supabase s3 modify security policies to allow shot thumbnails

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,27 @@
 import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
-
+    upload: {
+        config: {
+            breakpoints: {},
+            provider: '@strapi/provider-upload-aws-s3',
+            providerOptions: {
+                baseUrl: env('SUPABASE_S3_URL_PUBLIC'),
+                s3Options: {
+                    credentials: {
+                        accessKeyId: env('SUPABASE_S3_ACCESS_KEY'),
+                        secretAccessKey: env('SUPABASE_S3_SECRET_KEY'),
+                    },
+                    region: 'us-east-1',
+                    endpoint: env('SUPABASE_S3_URL'),
+                    forcePathStyle: true,
+                    params: {
+                        Bucket: env('SUPABASE_S3_BUCKET'),
+                    },
+                },
+            },
+        },
+    },
     email: {
         config: {
             provider: 'strapi-provider-email-resend',
--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,6 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const config = ({ env }) => ({
+    upload: {
+        config: {
+            breakpoints: {},
+            provider: '@strapi/provider-upload-aws-s3',
+            providerOptions: {
+                baseUrl: env('SUPABASE_S3_URL_PUBLIC'),
+                s3Options: {
+                    credentials: {
+                        accessKeyId: env('SUPABASE_S3_ACCESS_KEY'),
+                        secretAccessKey: env('SUPABASE_S3_SECRET_KEY'),
+                    },
+                    region: 'us-east-1',
+                    endpoint: env('SUPABASE_S3_URL'),
+                    forcePathStyle: true,
+                    params: {
+                        Bucket: env('SUPABASE_S3_BUCKET'),
+                    },
+                },
+            },
+        },
+    },
     email: {
         config: {
             provider: 'strapi-provider-email-resend',
